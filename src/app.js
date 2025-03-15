6e3ac6676f5cc8c74d42a92f889971aac5ad7b3a
Antonino <LAZARONUNEZANTONINO@GMAIL.COM>
2025-03-15 23:10:34 +0000

    Ordenando el array según el dominio

--- src/app.js
+++ src/app.js
@@ -1,35 +1,35 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  const pronoun = ['their', 'this', 'that','antonino'];
-  const adj = ['lazy', 'strong', 'lazaro','adorable'];
-  const noun = ['wizard', 'ninja','esuncrack', 'pirate'];
-  const extensions = ['.com','.edu', '.gov', '.info', '.biz', '.us'];
+window.onload = function () {
+  const pronoun = ['their', 'this', 'that', 'antonino'];
+  const adj = ['lazy', 'strong', 'lazaro', 'adorable'];
+  const noun = ['wizard', 'ninja', 'esuncrack', 'pirate'];
 
-  const generateDomain=(pronoun,adj,noun)=>{
+  const generateDomain = (pronoun, adj, noun) => {
     const arrayOfDomains = [];
-    let line;
-     for (let i = 0;i<pronoun.length;i++){
-       for (let j = 0;j<adj.length;j++){
-         for (let k = 0;k<noun.length;k++){
-           line = pronoun[i]+adj[j]+noun[k];
-           if (arrayOfDomains.includes(line)){
-           }else arrayOfDomains.push(line);
-         }
-       }
-     }
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          arrayOfDomains.push(pronoun[i] + adj[j] + noun[k]);
+        }
+      }
+    }
     return arrayOfDomains;
-  }
-  const appendDotCom = (value) => {
-    return `<li class="list-group-item bg-light">${value}${extensions[Math.floor(Math.random() * extensions.length)]}</li>`;
   };
-  const completedArrayOfDomains = generateDomain(pronoun,adj,noun).map(appendDotCom);
-  console.log(completedArrayOfDomains);
-  document.getElementById("domaintext").innerHTML = completedArrayOfDomains.join("");
+
+  // Transforma el string insertando un punto antes de los dos últimos caracteres.
+  const transformarString = str => str.slice(0, -2) + '.' + str.slice(-2);
+
+  // Encadenamos los métodos para transformar, ordenar por dominio (lo que va después del punto)
+  // y finalmente envolver en una etiqueta <li> para asignar al HTML.
+  const domainsOrdered = generateDomain(pronoun, adj, noun)
+    .map(transformarString)
+    .sort((a, b) => a.split('.').pop().localeCompare(b.split('.').pop()))
+    .map(value => `<li class="list-group-item bg-light">${value}</li>`);
+
+  document.getElementById("domaintext").innerHTML = domainsOrdered.join("");
 };
