1aa04749a9e2fd8318d57e714b3429b788cd5955
Antonino <LAZARONUNEZANTONINO@GMAIL.COM>
2025-03-15 17:51:10 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const pronoun = ['the', 'our', 'my', 'your', 'her', 'his', 'its', 'their', 'this', 'that'];
+  const adj = ['great', 'big', 'crazy', 'fast', 'tiny', 'shiny', 'bright', 'fancy', 'lazy', 'strong', 'adorable'];
+  const noun = ['jogger', 'racoon', 'dog', 'cat', 'turtle', 'unicorn', 'dragon', 'rabbit', 'bear', 'wizard', 'ninja', 'pirate'];
+  const extensions = ['.com', '.es', '.net', '.org', '.io', '.edu', '.gov', '.info', '.biz', '.us'];
+  let completedArrayOfDomains = [];
+
+  let generateDomain=(pronoun,adj,noun)=>{
+    let arrayOfDomains = [];
+    let line;
+    for (let i = 0;i<pronoun.length;i++){
+      for (let j = 0;j<adj.length;j++){
+        for (let k = 0;k<noun.length;k++){
+          line = pronoun[i]+adj[j]+noun[k];
+          if (arrayOfDomains.includes(line)){
+          }else arrayOfDomains.push(line);
+        }
+      }
+    }
+    return arrayOfDomains;
+  }
+  let appendDotCom = (value) => {
+    return `${value}${extensions[Math.floor(Math.random() * extensions.length)]}`;
+  };
+  completedArrayOfDomains = generateDomain(pronoun,adj,noun).map(appendDotCom);
+  console.log(completedArrayOfDomains);
 };
