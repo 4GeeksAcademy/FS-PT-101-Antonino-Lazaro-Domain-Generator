fdfff3f7f6a3b3e7517b47a3d1820efeb66a7d58
Antonino <LAZARONUNEZANTONINO@GMAIL.COM>
2025-03-15 19:53:15 +0000

    Second Commit

--- src/app.js
+++ src/app.js
@@ -7,29 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  const pronoun = ['the', 'our', 'my', 'your', 'her', 'his', 'its', 'their', 'this', 'that'];
-  const adj = ['great', 'big', 'crazy', 'fast', 'tiny', 'shiny', 'bright', 'fancy', 'lazy', 'strong', 'adorable'];
-  const noun = ['jogger', 'racoon', 'dog', 'cat', 'turtle', 'unicorn', 'dragon', 'rabbit', 'bear', 'wizard', 'ninja', 'pirate'];
-  const extensions = ['.com', '.es', '.net', '.org', '.io', '.edu', '.gov', '.info', '.biz', '.us'];
-  let completedArrayOfDomains = [];
+  const pronoun = ['their', 'this', 'that','antonino'];
+  const adj = ['lazy', 'strong', 'lazaro','adorable'];
+  const noun = ['wizard', 'ninja','esuncrack', 'pirate'];
+  const extensions = ['.com','.edu', '.gov', '.info', '.biz', '.us'];
 
-  let generateDomain=(pronoun,adj,noun)=>{
-    let arrayOfDomains = [];
+  const generateDomain=(pronoun,adj,noun)=>{
+    const arrayOfDomains = [];
     let line;
-    for (let i = 0;i<pronoun.length;i++){
-      for (let j = 0;j<adj.length;j++){
-        for (let k = 0;k<noun.length;k++){
-          line = pronoun[i]+adj[j]+noun[k];
-          if (arrayOfDomains.includes(line)){
-          }else arrayOfDomains.push(line);
-        }
-      }
-    }
+     for (let i = 0;i<pronoun.length;i++){
+       for (let j = 0;j<adj.length;j++){
+         for (let k = 0;k<noun.length;k++){
+           line = pronoun[i]+adj[j]+noun[k];
+           if (arrayOfDomains.includes(line)){
+           }else arrayOfDomains.push(line);
+         }
+       }
+     }
     return arrayOfDomains;
   }
-  let appendDotCom = (value) => {
+  const appendDotCom = (value) => {
     return `${value}${extensions[Math.floor(Math.random() * extensions.length)]}`;
   };
-  completedArrayOfDomains = generateDomain(pronoun,adj,noun).map(appendDotCom);
+  const completedArrayOfDomains = generateDomain(pronoun,adj,noun).map(appendDotCom);
   console.log(completedArrayOfDomains);
 };
